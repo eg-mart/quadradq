78f6db844eace1c952433e5cea770543da897344
eg-mart <marteg71@gmail.com>
2023-08-22 20:25:02 +0300

    add function documentation

--- equation_solver.h
+++ equation_solver.h
@@ -17,8 +17,39 @@ struct roots {
 	enum nroots n;
 };
 
+/**
+* Checks if two doubles are closer then DBL_EPSILON to each other.
+* 
+* @param [in] x first double value (must be finite)
+* @param [in] y second double value (must be finite)
+* 
+* @return 1 if values are close, 0 otherwise
+*/
 int is_equal(double x, double y);
+
+/**
+* Solves a square equation of the form ax^2 + bx + c = 0.
+*
+* @param [in]  coeffs a sctruct containing a, b and c coefficients
+* @param [out] roots  a pointer to a struct containing roots x1, x2 and a number of roots
+*/
 void solve_quadratic(struct coefficients coeffs, struct roots *roots);
+
+/**
+* Solves a linear equation of the form ax + b = 0.
+* 
+* @param [in]  a     a-coefficient (must be finite)
+* @param [in]  b     b-coefficient (must be finite)
+* @param [out] roots a pointer to a struct containing roots x1, x2 and a number of roots
+*/
 void solve_linear(double a, double b, struct roots *roots);
+
+/**
+* Calculates a discriminant for a given equation ax^2 + bx + c = 0.
+*
+* @param [in] coeffs a struct containing a, b and c coefficients
+*
+* @return a discriminant value
+*/
 double calc_discrim(struct coefficients coeffs);
 

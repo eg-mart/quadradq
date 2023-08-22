49842374027e0fa8fa56e68d6ec0d44986a5d6e6
eg-mart <marteg71@gmail.com>
2023-08-22 17:40:47 +0300

    add file input/output

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 #include <assert.h>
 #include <stdlib.h>
 
-enum solution_stat {
-	ERR,
-	NO_SOL,
-	ONE_SOL,
-	TWO_SOL,
-	INF_SOL,
+enum error {
+	NORM,
+	FILE_ENDED,
+	ERR_FILE_OPEN,
+	ERR_FILE_WRITE,
+	ERR_FORMAT,
+	ERR_UNKNOWN
 };
-
+	
 struct coefficients {
 	double a;
 	double b;
@@ -30,45 +31,113 @@ int is_equal(double x, double y);
 void solve_quadratic(struct coefficients coeffs, struct roots *roots);
 void solve_linear(double a, double b, struct roots *roots);
 double calc_discrim(struct coefficients coeffs);
+enum error input_coefficients(FILE *input, struct coefficients *coeffs);
+enum error output_roots(FILE *output, struct roots roots);
+int print_error(FILE *error, enum error err_code);
 
 int main() {
 	struct coefficients coeffs = { NAN, NAN, NAN };
 	struct roots roots = { NAN, NAN, 0 };
+	FILE *input, *output, *error;
 
-	char buf[64];
-	if (fgets(buf, sizeof(buf), stdin)) {
-		int n = 0;
-		if (sscanf(buf, "%lf %lf %lf %n", &coeffs.a, &coeffs.b, &coeffs.c, &n) != 3 ||
-				buf[n] != '\0') {
-			fprintf(stderr, "Произошла ошибка ввода");
-			exit(1);
-		}
-	} else {
-		fprintf(stderr, "Произошла ошибка ввода");
-		exit(1);
+	if ((output = fopen("output.txt", "a")) == NULL) {
+		print_error(stderr, ERR_FILE_OPEN);
+		return 1;
+	}
+	error = output;
+
+	if ((input = fopen("input.txt", "r")) == NULL) {
+		print_error(stderr, ERR_FILE_OPEN);
+		return 1;
 	}
 
-	solve_quadratic(coeffs, &roots);
+	enum error input_status = NORM, output_status = NORM;
 
-	switch (roots.n) {
-		case 0:
-			printf("Решений не найдено.");
-			break;
-		case 1:
-			printf("Найдено 1 решение: %.4lf", roots.x1);
-			break;
-		case 2:
-			printf("Найдено 2 решения: %.4lf %.4lf", roots.x1, roots.x2);
-			break;
-		case INF_ROOTS:
-			printf("Найдено бесконечно много решений.");
-			break;
-		default:
-			fprintf(stderr, "Произошла ошибка!");
-			exit(1);
+	while (input_status != FILE_ENDED) {
+		input_status = input_coefficients(input, &coeffs);
+		print_error(error, input_status);
+
+		if (input_status == NORM) {
+			solve_quadratic(coeffs, &roots);
+
+			output_status = output_roots(output, roots);
+			print_error(error, output_status);
+		}
 	}
 
 	return 0;
+}
+
+int print_error(FILE *error, enum error err_code)
+{
+	switch (err_code) {
+		case NORM:
+			return 0;
+		case FILE_ENDED:
+			return 0;
+		case ERR_FILE_OPEN:
+			fprintf(error, "[ERROR] error opening a file\n");
+			break;
+		case ERR_FILE_WRITE:
+			fprintf(error, "[ERROR] error writing to a file\n");
+			break;
+		case ERR_FORMAT:
+			fprintf(error, "[ERROR] wrong input format\n");
+			break;
+		case ERR_UNKNOWN:
+			fprintf(error, "[ERROR] an unknown error occured\n");
+			break;
+		default:
+			fprintf(error, "[ERROR] an unknown error occured\n");
+	}
+
+	if (ferror(error))
+		fprintf(stderr, "[ERROR] error writing to an error file\n");
+
+	return 1;
+}
+
+enum error input_coefficients(FILE *input, struct coefficients *coeffs)
+{
+	int scanned = fscanf(input, "%lf %lf %lf ", &coeffs->a, &coeffs->b, &coeffs->c);
+
+	int tmp = '\n';
+	if (scanned != 3 && scanned != EOF) {
+		while ((tmp = getc(input)) != '\n' && tmp != EOF)
+			;
+		return ERR_FORMAT;
+	}
+
+	if (scanned == EOF || tmp == EOF) {
+		return FILE_ENDED;
+	}
+
+	return NORM;
+}
+
+enum error output_roots(FILE *output, struct roots roots)
+{
+	switch (roots.n) {
+		case 0:
+			fprintf(output, "no roots\n");
+			break;
+		case 1:
+			fprintf(output, "%lg\n", roots.x1);
+			break;
+		case 2:
+			fprintf(output, "%lg %lg\n", roots.x1, roots.x2);
+			break;
+		case INF_ROOTS:
+			fprintf(output, "infinite roots\n");
+			break;
+		default:
+			return ERR_UNKNOWN;
+	}
+	
+	if (ferror(output))
+		return ERR_FILE_WRITE;
+
+	return NORM;
 }
 
 void solve_quadratic(struct coefficients coeffs, struct roots *roots)

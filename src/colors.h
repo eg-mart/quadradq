d711bf764520af81fe4111d33878ca9f5a005203
eg-mart <marteg71@gmail.com>
2023-08-25 11:04:10 +0300

    add include guard to colors.h

--- src/colors.h
+++ src/colors.h
@@ -1,5 +1,10 @@
+#ifndef COLORS_LIST
+#define COLORS_LIST
+
 static const char *RESET_COLOR = "\033[0m";
 static const char *RED = "\033[0m\033[31m";
 static const char *GREEN = "\033[0m\033[32m";
 static const char *YELLOW = "\033[0m\033[33m";
 static const char *BLUE = "\033[0m\033[34m";
+
+#endif
